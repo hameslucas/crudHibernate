2cd144c3407dc124db5a5743b45070c75ad0cc63
L. Hames <L. Hames@LAPTOP-2SER9IS0>
2022-04-10 19:55:52 -0300

    first commit

---
+++ src/main/java/br/com/senac/dao/UsuarioDaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.senac.dao;
+
+import br.com.senac.entidade.Usuario;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author L. Hames
+ */
+public class UsuarioDaoImpl extends BaseDaoImpl<Usuario, Long> implements UsuarioDao{
+
+    @Override
+    public Usuario pesquisarPorId(Long id, Session sessao) throws HibernateException {
+        return sessao.get(Usuario.class, id);
+    }
+    
+}
